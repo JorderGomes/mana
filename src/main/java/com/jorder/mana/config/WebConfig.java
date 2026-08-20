a7ac24b11fe704efe8d6d1a237c270b0f4a1cb89
JorderGomes <jordernoveau@gmail.com>
2026-08-20 14:20:43 -0300

    config: change CORS configuration to RepositoryRestConfigurer

--- src/main/java/com/jorder/mana/config/WebConfig.java
+++ src/main/java/com/jorder/mana/config/WebConfig.java
@@ -2,18 +2,22 @@ package com.jorder.mana.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+//@EnableWebMvc
+public class WebConfig implements RepositoryRestConfigurer {
     @Value("${allowed_origin}")
     private String allowed_origin;
 
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**")
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        cors.addMapping("/**")
                 .allowedOrigins(allowed_origin)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
